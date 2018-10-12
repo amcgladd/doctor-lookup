163e5ef770c8e1e2ba2cf9e0133d1487640754d1
andrew <amcgladd@gmail.com>
2018-10-12 16:41:47 -0700

    add the ability to handle values of website and availability

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import DoctorSearch from './DoctorSearch.js';
 function parseResults(data){
   let finalOutput = [];
   data.forEach(function(doctor){
-    console.log(doctor.profile.first_name,doctor.profile.last_name);
     //create a div with doctor information
     let doctorDiv =
     `<div>` +
@@ -19,23 +18,25 @@ function parseResults(data){
     `<p>` +
     doctor.practices[0].visit_address.city + ` ` + doctor.practices[0].visit_address.state + ` ` + doctor.practices[0].visit_address.zip + `</p>`+
     `<p>Phone: ` +
-    doctor.practices[0].phones[0].number + `</p>`+
-    // `<a href=>` +
-    // doctor.practices[0].website + `</a>`+
-    `<p>Accepting New Patients? ` +
-    doctor.practices[0].accepts_new_patients + `</p>`+
-    `</div>`;
+    doctor.practices[0].phones[0].number + `</p>`;
+
+    //check for website
+    if (doctor.practices[0].hasOwnProperty('website')) {
+      doctorDiv += `<a href=`+ doctor.practices[0].website + `>Wesbite</a>`;
+    } 
+
+    //checks status of accepting patients
+    if(doctor.practices[0].accepts_new_patients == true) {
+      doctorDiv += `<p>Accepting New Patients</p></div>`;
+    } else {
+      doctorDiv += `<p>Not Currently Accepting New Patients</p></div>`;
+    }
+
     //push div to finalOuput
     finalOutput.push(doctorDiv);
   });
   return finalOutput;
 }
-
-//testing if the obect has pproperties
-// console.log(arr.some(item => item.name === 'Blofeld'));
-// console.log(arr.some(item => item.name === 'Blofeld2'));
-
-
 
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
